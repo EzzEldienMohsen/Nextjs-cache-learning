a9c7d95278506fed6fb78a393f1e6a0e5569adc1
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-14 15:24:36 +0200

    all pages cache controlling

--- app/messages/page.js
+++ app/messages/page.js
@@ -10,17 +10,25 @@ export const revalidate = 15; // it does the same functionality as being putted 
 // export const dynamic = "force-dynamic"
 // and this has the same effect of the cache set up in the header with the value of no store.
 // If you change the value to force-static the app will never fetch any new data
+// when using the dynamic constant it can turn the page into dynamic page
+// it will re render the page and refetch the data every time a request sent in the production
+// after using npm run build
 
 export default async function MessagesPage() {
   // unstable_noStore(); it is used as dynamic constant with force-dynamic value but it is just for this component
   const response = await fetch(
     'http://localhost:8080/messages'
-    // , {
+    //  , {
     //   // cache: 'no-store',
-    //   next: {
+    // next: {
     //     revalidate: 5, //number of second to use the data then revalidate
-    //   },
-    // }
+    // tags:["any tag"]
+    //  these tags are used with revalidate tags as it is connected to cache
+    // you can set it in the request header and you can set the same tag into multiple requests
+    // and set multiple tags to the same request
+    // it works just like revalidate path but with tags instead of pages ans layouts
+    //    },
+    //  }
   );
   const messages = await response.json();
 
